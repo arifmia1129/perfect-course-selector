a4f98cab6e5e53fe49bdec2cf3068505bf1ec4e7
arifvtti <arif.vtti@gmail.com>
2022-03-25 23:23:24 +0600

    destructuring

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -1,11 +1,10 @@
 import React from 'react';
-const Course = (props) => {
-    console.log(props);
+const Course = ({ course }) => {
+    const { name, price, img } = course;
+    console.log(name);
     return (
         <div>
-            <img src={props.course.img} alt="" />
-            <img src="" alt="" />
-            <h1>{props.course.name}</h1>
+
         </div>
     );
 };
