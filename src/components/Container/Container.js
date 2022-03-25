889563e1cc9aa78dcb1729fab9de7e37732107c0
arifvtti <arif.vtti@gmail.com>
2022-03-26 00:30:52 +0600

    loaded data with card design

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -5,29 +5,28 @@ import "./Container.css";
 
 const Container = () => {
     const courses = [
-        { id: 1, name: "C Programming Course", price: 1500, img: "images/c.png" },
-        { id: 2, name: "C# Programming Course", price: 2500, img: "images/c-s.jpg" },
-        { id: 3, name: "C++ Programming Course", price: 3000, img: "images/c++.png" },
-        { id: 4, name: "Java Programming Course", price: 3500, img: "images/java.png" },
+        { id: 1, name: "C Course", price: 1500, img: "images/c.png", duration: "4 month", medium: "Online" },
+        { id: 2, name: "C# Course", price: 2500, img: "images/c-s.jpg", duration: "4 month", medium: "Online" },
+        { id: 3, name: "C++ Course", price: 3000, img: "images/c++.png", duration: "4 month", medium: "Online" },
+        { id: 4, name: "Java Course", price: 3500, img: "images/java.png", duration: "4 month", medium: "Online" },
         {
-            id: 5, name: "Javascript Programming Course", price:
+            id: 5, name: "Javascript Course", price:
                 3500, img: "images/javascript.jpg"
+            , duration: "4 month", medium: "Online"
         },
-        { id: 6, name: "Python Programming Course", price: 4000, img: "images/python.jpg" },
-        { id: 7, name: "Go Programming Course", price: 4500, img: "images/go.png" },
-        { id: 8, name: "HTML Course", price: 1500, img: "images/html.png" },
-        { id: 9, name: "CSS Course", price: 1500, img: "images/CSS.png" },
-        { id: 10, name: "React Course", price: 1500, img: "images/react.png" },
-        { id: 11, name: "Web Development Course", price: 5500, img: "images/web.png" },
-        { id: 12, name: "Machine Learning Course", price: 6000, img: "images/machine.png" }
+        { id: 6, name: "Python Course", price: 4000, img: "images/python.jpg", duration: "4 month", medium: "Online" },
+        { id: 7, name: "Go Programming Course", price: 4500, img: "images/go.png", duration: "4 month", medium: "Online" },
+        { id: 8, name: "HTML Course", price: 1500, img: "images/html.png", duration: "4 month", medium: "Online" },
+        { id: 9, name: "CSS Course", price: 1500, img: "images/CSS.png", duration: "4 month", medium: "Online" },
+        { id: 10, name: "React Course", price: 1500, img: "images/react.png", duration: "4 month", medium: "Online" },
+        { id: 11, name: "Web Development Course", price: 5500, img: "images/web.png", duration: "4 month", medium: "Online" },
+        { id: 12, name: "Machine Learning Course", price: 6000, img: "images/machine.png", duration: "4 month", medium: "Online" }
     ];
     return (
         <div className='container'>
             <Row>
                 <Col md={8} sm={12}>
                     <div className='course-container'>
-                        <h1>Img</h1>
-                        <img src="../../images/c#.jpg" alt="" />
                         {
                             courses.map(course => <Course key={course.id} course={course}></Course>)
                         }
--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+import "./Course.css";
 const Course = ({ course }) => {
-    const { name, price, img } = course;
+    const { duration, medium, name, price, img } = course;
 
     return (
-        <div>
-
+        <div >
+            <Card className='card'>
+                <Card.Img variant="top" src={img} />
+                <Card.Body>
+                    <Card.Title>{name}</Card.Title>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    <ListGroupItem>{price} <span>৳</span></ListGroupItem>
+                    <ListGroupItem>Duration: {duration}</ListGroupItem>
+                    <ListGroupItem>Class system: {medium}</ListGroupItem>
+                </ListGroup>
+                <Card.Body>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                </Card.Body>
+            </Card>
         </div>
     );
 };
